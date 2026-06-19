5539712ea82ff2ecf732788615018697f23bcd66
POUPON CEDRIC KOULEDOU <cedric.kouledou@chem.loc>
2026-06-19 11:39:50 +0100

    le code juste apresla connexion , risque car je n'ai pas terminer

--- bibliotheque/config/cors.php
+++ bibliotheque/config/cors.php
@@ -1,10 +1,11 @@
 <?php
 
 return [
+    // Chemins où CORS s'applique
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
-
+    // Origines autorisées (ton frontend)
     'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173')],
 
     'allowed_origins_patterns' => [],
@@ -14,6 +15,7 @@ return [
     'exposed_headers' => [],
 
     'max_age' => 0,
-
+    // Autorise les cookies cross-origin 
+    //Sans supports_credentials => true, le navigateur refuse d'envoyer les cookies. 
     'supports_credentials' => true,
 ];
