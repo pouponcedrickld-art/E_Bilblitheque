4701fc933f8c2da22da052efa7d2cf61c7a0150e
POUPON CEDRIC KOULEDOU <cedric.kouledou@chem.loc>
2026-06-29 10:55:01 +0100

    fix :correction du nom du fichiers des routes du RH

--- bibliotheque/routes/api.php
+++ bibliotheque/routes/api.php
@@ -7,5 +7,5 @@ require __DIR__.'/api/auth.php';//tous ce qui concerne la connexion
 require __DIR__.'/api/public.php';//toutes les routes qui sont publics
 require __DIR__.'/api/authenticated.php';//toutes les routes lier  a une personnne déjà connecter 
 require __DIR__.'/api/manager.php';//toutes les routes lier au module de des responsables de demandes 
-require __DIR__.'/api/hr.php';//toutes les routes lier au rh 
+require __DIR__.'/api/rh.php';//toutes les routes lier au rh 
 require __DIR__.'/api/admin.php';//toutes les routes lier a l'admin 
